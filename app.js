3cf4a5109d200cba786155ddcb403ac5634fcd45
valentavo <valentin.tavo@gmail.com>
2023-05-24 22:34:45 -0300

    actualizacion de servidor

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express()
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-    console.log("servidor corriendo en el puerto 3000")
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
 })
 
 app.get("/", (req, res) => {
