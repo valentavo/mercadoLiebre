75f3d8fe1945fb53b2ef2931b66129e26ba90e30
valentavo <valentin.tavo@gmail.com>
2023-05-24 21:36:50 -0300

    entrega

---
+++ mercadoLiebre.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const path = require("path") 
+const app = express()
+
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+    console.log("servidor corriendo en el puerto 3000")
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+
+
